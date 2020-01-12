f3fdd6e179945a94c1691e0e48cb4059379bf6e6
Leifytwig <Leifytwig123@yahoo.com>
2020-01-12 12:50:44 -0600

    Add Arrow class

---
+++ JoustusEngine/Arrow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Arrow
+{
+    ArrowType _arrowType;
+
+	public Arrow(ArrowType arrowType)
+	{
+        _arrowType = arrowType;
+	}
+
+    public ArrowType GetType()
+    {
+        return _arrowType;
+    }
+
+    public ArrowType SetType(ArrowType arrowType)
+    {
+        _arrowType = arrowType;
+    }
+
+    public static enum ArrowType
+    {
+        Single,
+        Double,
+        Triple,
+        Bomb,
+        Conveyor
+    }
+}
