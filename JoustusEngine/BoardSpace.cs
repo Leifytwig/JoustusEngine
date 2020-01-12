e5489f85052dca6fdd35a39f5a13fbd6b4c36bb1
Leifytwig <Leifytwig123@yahoo.com>
2020-01-12 13:27:27 -0600

    Add BoardSpace class

---
+++ JoustusEngine/BoardSpace.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace JoustusEngine
+{
+    public class BoardSpace
+    {
+        SpaceType _spaceType;
+        Card _card;
+        bool _hasGem;
+
+        public BoardSpace() : this(SpaceType.Open) { }
+
+        public BoardSpace(SpaceType spaceType) : this(spaceType, false) { }
+
+        public BoardSpace(SpaceType spaceType, bool hasGem) : this(spaceType, hasGem, null) { }
+
+        public BoardSpace(SpaceType spaceType, bool hasGem, Card card)
+        {
+            _spaceType = spaceType;
+            _hasGem = hasGem;
+            _card = card;
+        }
+
+        public SpaceType GetSpaceType()
+        {
+            return _spaceType;
+        }
+
+        public bool GetHasGem()
+        {
+            return _hasGem;
+        }
+
+        public Card GetCard()
+        {
+            return _card;
+        }
+
+        public void SetSpaceType(SpaceType spaceType)
+        {
+            _spaceType = spaceType;
+        }
+
+        public void SetHasGem(bool hasGem)
+        {
+            _hasGem = hasGem;
+        }
+
+        public void SetCard(Card card)
+        {
+            _card = card;
+        }
+    }
+
+    public enum SpaceType
+    {
+        Open,
+        Grave,
+        Hole,
+        HoleFilled,
+        Rock,
+        Closed
+    }
+}
